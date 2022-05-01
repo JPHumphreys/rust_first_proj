5781e052928ad387145c6157ed116223f8a51593
DECENTRALISED <jph.code@pm.me>
2022-05-01 16:42:39 +0100

    made into functions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 fn main() {
+    print_lat_long();
+    print_strings();
+}
+
+fn print_lat_long(){
     let location = ("location1", 41.407, -81.2);
     let (name, latitude, longitude) = location;
     println!("Location name: {}, lat: {}, lon: {}",
      name, latitude, longitude);
+}
+
+fn print_strings(){
+
 }
