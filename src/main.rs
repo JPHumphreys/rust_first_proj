306573fe9e0cf5fe9e309c514be182224adeb2c8
DECENTRALISED <jph.code@pm.me>
2022-05-01 16:48:36 +0100

    commented out the frist print

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,7 @@ fn main() {
 fn print_lat_long(){
     let location = ("location1", 41.407, -81.2);
     let (name, latitude, longitude) = location;
-    println!("Location name: {}, lat: {}, lon: {}",
-     name, latitude, longitude);
+    //println!("Location name: {}, lat: {}, lon: {}", name, latitude, longitude);
 }
 
 fn print_strings(){
