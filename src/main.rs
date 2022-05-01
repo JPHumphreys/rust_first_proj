0b9cc287a0b2ec03f7e457a14a32434fc8a48990
DECENTRALISED <jph.code@pm.me>
2022-05-01 16:37:18 +0100

    created my first touple

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
-    println!("Hello, world!");
+    let location = ("location1", 41.407, -81.2);
+    println!("Location name: {}, lat: {}, lon: {}",
+     location.0, location.1, location.2);
 }
