6bc3bc1673fcaf4b1602f29824f09b2418325e7d
DECENTRALISED <jph.code@pm.me>
2022-05-02 11:40:04 +0100

    added match example

---
+++ src/match.rs
@@ -0,0 +1,8 @@
+fn basic(){
+    let animal = "Duck";
+    match animal{
+        "Duck" => print!("Quack"),
+        "Dog" => print!("Woof"),
+        _ => print!("Beep Boop")
+    }
+}
--- src/option.rs
+++ src/option.rs
@@ -1,3 +1,11 @@
 fn basic(){
+    let phrase = String::from("Duck Airlines");
+    let letter = phrase.chars().nth(5);
     
+    let value = match letter{
+        Some(character) => character.to_string(),
+        None => String::from("No Value")
+    };
+
+    print!("{}", value);
 }
