c84546aabfbfbfce850c203c1532c86571fbb635
DECENTRALISED <jph.code@pm.me>
2022-05-02 12:34:32 +0100

    added loops

--- src/loop.rs
+++ src/loop.rs
@@ -6,4 +6,23 @@ fn basic(){
             break;
         }
     }
+
+    while counter <= 10{
+        counter += 10;
+        println!("hello");
+    }
+
+    for index in 1..11{
+        println!("1-10");
+    }
+
+    for index in 1..=10{
+        print!("1-10");
+    }
+
+    let aircrafts = ["1","2","3"];
+
+    for aircraft in aircrafts.iter(){
+        println!(aircraft);
+    }
 }
