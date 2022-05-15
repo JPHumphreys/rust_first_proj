1f4970958e2dec13cf1f65fb1def98a4777dff8d
DECENTRALISED <jph.code@pm.me>
2022-05-15 19:11:37 +0100

    added an explicit lifetime example

--- src/ownership.rs
+++ src/ownership.rs
@@ -11,3 +11,18 @@ fn ownership(){
 
     println!("{}", original);
 }
+
+fn lifetimes(){
+    let value_one = 24;
+    let value_two = 67;
+    let value = explicit_lifetime(&value_one, &value_two);
+}
+
+fn explicit_lifetime<'a>(p1: &'a i32, p2: &'a i32) -> &'a i32{
+    if p1 > p2{
+        p1
+    }
+    else{
+        p2
+    }
+}
